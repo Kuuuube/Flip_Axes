b4d146fe89e1afa35786b2a20aa26b13ed8d1b08
kuuube <hexagonisalie@gmail.com>
2022-04-21 10:32:29 -0400

    Feat: Add plugin

---
+++ Flip_Axes/Flip_Axes.cs
@@ -0,0 +1,54 @@
+﻿using OpenTabletDriver.Plugin.Attributes;
+using OpenTabletDriver.Plugin.Tablet;
+using OpenTabletDriver.Plugin.Output;
+using OpenTabletDriver.Plugin;
+using System;
+using System.Numerics;
+
+namespace Flip_Axes
+{
+    [PluginName("Flip Axes")]
+    public class Flip_Axes : Flip_Axes_Base
+    {
+
+        public Vector2 Flip(Vector2 input)
+        {
+            if (Flip_X)
+            {
+                input.X = input.X * -1;
+            }
+            if (Flip_Y)
+            {
+                input.Y = input.Y * -1;
+            }
+            return input;
+        }
+
+        public override event Action<IDeviceReport> Emit;
+
+        public override void Consume(IDeviceReport value)
+        {
+            if (value is ITabletReport report)
+            {
+                report.Position = Filter(report.Position);
+                value = report;
+            }
+
+            Emit?.Invoke(value);
+        }
+
+        public Vector2 Filter(Vector2 input) => FromUnit(Flip(ToUnit(input)));
+
+        public override PipelinePosition Position => PipelinePosition.PostTransform;
+
+        [BooleanProperty("Flip X Axis", ""), ToolTip
+            ("Flip Axes:\n\n" +
+            "Flip X Axis: Flips the X axis coordinates.")]
+        public bool Flip_X { set; get; }
+
+        [BooleanProperty("Flip Y Axis", ""), ToolTip
+        ("Flip Axes:\n\n" +
+        "Flip X Axis: Flips the Y axis coordinates.")]
+        public bool Flip_Y { set; get; }
+    }
+}
---
+++ Flip_Axes/Flip_Axes_Base.cs
@@ -0,0 +1,75 @@
+﻿using OpenTabletDriver;
+using OpenTabletDriver.Plugin;
+using OpenTabletDriver.Plugin.Attributes;
+using OpenTabletDriver.Plugin.DependencyInjection;
+using OpenTabletDriver.Plugin.Output;
+using OpenTabletDriver.Plugin.Tablet;
+using System;
+using System.Linq;
+using System.Numerics;
+
+namespace Flip_Axes
+{
+    public abstract class Flip_Axes_Base : IPositionedPipelineElement<IDeviceReport>
+    {
+        protected Vector2 ToUnit(Vector2 input)
+        {
+            if (outputMode is not null)
+            {
+                var display = outputMode?.Output;
+                var offset = (Vector2)(outputMode?.Output?.Position);
+                var shiftoffX = offset.X - (display.Width / 2);
+                var shiftoffY = offset.Y - (display.Height / 2);
+                return new Vector2(
+                    (input.X - shiftoffX) / display.Width * 2 - 1,
+                    (input.Y - shiftoffY) / display.Height * 2 - 1
+                    );
+            }
+            else
+            {
+                tryResolveOutputMode();
+                return default;
+            }
+        }
+
+        protected Vector2 FromUnit(Vector2 input)
+        {
+            if (outputMode is not null)
+            {
+                var display = outputMode?.Output;
+                var offset = (Vector2)(outputMode?.Output?.Position);
+                var shiftoffX = offset.X - (display.Width / 2);
+                var shiftoffY = offset.Y - (display.Height / 2);
+                return new Vector2(
+                    (input.X + 1) / 2 * display.Width + shiftoffX,
+                    (input.Y + 1) / 2 * display.Height + shiftoffY
+                );
+            }
+            else
+            {
+                return default;
+            }
+        }
+
+
+        [Resolved]
+        public IDriver driver;
+        private AbsoluteOutputMode outputMode;
+        private void tryResolveOutputMode()
+        {
+            if (driver is Driver drv)
+            {
+                IOutputMode output = drv.InputDevices
+                    .Where(dev => dev?.OutputMode?.Elements?.Contains(this) ?? false)
+                    .Select(dev => dev?.OutputMode).FirstOrDefault();
+
+                if (output is AbsoluteOutputMode absOutput)
+                    outputMode = absOutput;
+            }
+        }
+
+        public abstract event Action<IDeviceReport> Emit;
+        public abstract void Consume(IDeviceReport value);
+        public abstract PipelinePosition Position { get; }
+    }
+}
